e9f78662c023175c81a76f242f303ba4093f4386
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2023-04-10 02:55:22 +0300

    second  deleted

--- controller.py
+++ controller.py
@@ -2,64 +2,60 @@ import model
 import view
 
 def start():
+    model.open_file()
     choice = ''
     set_notes = []
     while True:
         choice = view.main_menu()
         match choice:
-            case 1 :
-                model.open_file()
-                view.information('\nФайл заметок успешно открыт\n')
-            case 2:
+            case 1:
                 view.show_notes()
-            case 3:
+            case 2:
                 new_note = list(view.create_new_note())
                 new_note_input = model.add_new_note(new_note)
-                view.information_data(new_note_input)
+                view.information(f'Введены данные : {new_note_input}')
                 view.information('Подтвердите ввод (1- записать, 2- отмена ввода) :')
                 yes_now = int(input(">"))
                 if(yes_now == 1):
                     model.write_new_note(new_note)
-                    view.information(f'\n Заметка \n{new_note_input}, \
-                                     \n успешно создана\n')
-            case 4:
+                    view.information(f' Заметка \n{new_note_input}, \
+                                     \n успешно создана')
+            case 3:
                 del_note = view.select_note('Введите номер удаляемой заметки: ')
                 note_ind = model.get_note(del_note)
                 if note_ind != []:
-                    # index = 0
-                    # if len(note_ind) > 1:
-                    #     index = view.number_index(note_ind)
-                    # else:
                     index = int(note_ind[0][0])
-                    if not index :
-                            break
                     confirm = view.delete_confirm(note_ind,index)
                     if confirm :
                         model.delete_note(index)
                         view.information(f'\nЗаметка с индексом {index - 1} успешно удалена\n')
                 else:
                     view.empty_request()
-            case 5 :
+            case 4 :
                 note = view.select_note('Введите номер изменяемой заметки: ')
                 note_ind = model.get_note(note)
                 if note_ind != [] :
-                    # index = 0
-                    # if len(note_ind) > 1:
-                    #     index = view.number_index(note_ind)
-                    # else:
                     index = int(note_ind[0][0])
-                    if not index :
-                            break
-                    updated_note= view.change_note()
+                    updated_note = view.change_note(note_ind)
                     model.update_note(index, list(updated_note))
-                    view.information(f'\n Заметка с индексом {index -1} успешно изменена\n')
+                    view.information(f'Заметка с индексом {index - 1} успешно изменена\n')
                 else:
                     view.empty_request()
-            case 6 :
+            case 5 :
                 find = view.find_note()
                 set_notes = model.get_note_date(find)
                 if len(set_notes) < 1:
-                    view.information('Указанная заметка не найдена')
+                    view.information('Заметки по дате не найдены')
+                else:
+                    for item in set_notes:
+                        view.information(f'№ заметки:{item[0] - 1} :{item[1][0]} {item[1][1]} {item[1][2]}')
+            case 6 :
+                find = view.select_note('Введите номер искомой заметки: ')
+                note_i = model.get_note(find)
+                if len(note_i) < 1:
+                    view.information('Заметка по номеру не найдена')
+                else:
+                    view.information(f'№ заметки:{note_i[0][0] - 1} :{note_i[0][1][0]} {note_i[0][1][1]} {note_i[0][1][2]}')                
             case 7 :
                 view.end_program()
                 model.sort_exit()
--- view.py
+++ view.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import datetime
 
-commands = ['Открыть файл заметок',
-            "Показать все заметки",
+commands = ["Показать все заметки",
             "Создать новую заметку",
             "Удалить заметку",
             "Изменить заметку",
-            "Найти заметку",
+            "Найти все заметки по дате",
+            "Найти заметку по номеру",
             "Завершить работу с заметками "]
+
 def main_menu() :
     print('Главное меню:')
     for i, item in enumerate(commands, 1):
@@ -24,30 +25,12 @@ def main_menu() :
 
 def show_notes():
     o = open('file_note.csv', mode = 'r')
-    o.close()
-    # data = pd.read_csv('phone_guide.csv',delimiter=',',encoding='cp1251')
     data = pd.read_csv('file_note.csv', delimiter = ',', encoding = 'utf-8')
+    o.close()
     if (not data.empty):
         print(data)
     else:
-        information("Файл пуст ")
-        
-# str(','.join(new_note))
-
-def number_index(note_ind:list):
-    list_index = []
-    for  index, row in note_ind:
-        list_index.append(index)
-    count = 0
-    while True:
-        if count == 3: 
-            return None
-        row_index = (int(input('Введите индекс искомой заметки :'))) 
-        if row_index in list_index:
-            return row_index
-        else:
-            print(f'Введен некорректный индекс{row_index}')
-            count += 1
+        information("Файл пуст ")      
        
 
 def input_error():
@@ -56,34 +39,24 @@ def input_error():
 def empty_request():
     print('Искомая заметка не найдена')
 
-# def show_find_contact(contact_dic):
-#     for  index, row in contact_dic:
-#         print(f' Индекс = {index}, контакт : {row}')
 
 def end_program(): 
-    print('До свидания. Программа завершена.')
+    print('Программа завершена.')
 
 def create_new_note():
     date_note = def_date()
-    # last_name = input('Введите фамилию :')
-
     header = input('Введите заголовок заметки :')
-    note_body = input('Введите текст заметки:')
-    # phone = input('Введите телефон:')
-    # comment = input('Введите адрес:')
-    return  date_note, header, note_body  #, phone, comment
+    note_body = input('Введите текст заметки:')  
+    return  date_note, header, note_body 
 
 def def_date(): 
     while True:
         date_d = input("Укажите: 1 - текущая дата, или 2 - ввод произвольной даты ")
         match date_d:
             case '1' :
-                d = datetime.date.today()
-                return str(d.year) + "/" + date_corr(str(d.month)) + "/" + date_corr(str(d.day))
+                d = str(datetime.datetime.now().strftime("%Y.%m.%d %H:%M:%S"))
+                return d  
             case '2' :
-                # d = input ("Введите год (гггг):")        
-                # d = d + input ("Введите месяц (01, 02 ... 12):")  
-                # d = d + input ("Введите число (01, 02 ... 31):") 
                 d = input_date()
                 right = input(f" Введена дата : {d}. Подтвердите корректность ввода (y/n):")
                 if (right in 'yYнН'):
@@ -93,43 +66,31 @@ def def_date():
 
                 
 def date_corr(d:str):
-    if len(d) == 1 : d = "0" + d
-    # print(" Введено : " + d)
-        # return  ('0' + d)
-    return d
+    return d if len(d) > 1 else ("0" + d)
+    
 
 def input_date():
-    d = input ("Введите год (гггг):")        
-    d = d + "/" + date_corr(input("Введите месяц (01, 02 ... 12):"))  
-    d = d + "/" + date_corr(input("Введите число (01, 02 ... 31):"))
-    # right = input(" Введена дата " + {d} + ". Подтвердите корректность ввода (y/n):")
-    #             if (right in 'yYнН'):
+    d =  input ("Введите год (гггг):")[:4] 
+    d = d + "." + date_corr(input("Введите месяц (01, 02 ... 12):"))[:2]  
+    d = d + "." + date_corr(input("Введите число (01, 02 ... 31):"))[:2]
     return d
 
 def find_note():
-    information_data('Введите дату искомой записи :\n')
+    information('Введите дату искомой записи :\n')
     while True:
         d = input_date()
         right = input(f" Введена дата : {d}. Подтвердите корректность ввода (y/n):")
         if (right in 'yYнН'):
             return d
-    #             if (right in 'yYнН'):
-    # return find
 
 
-# def delete_contact():
-#     delete = input('Введите удаляемый контакт')
-#     return delete
-
-
-def delete_confirm(note_ind:list,index:int):
+def delete_confirm(note_ind:list, index:int):
     if index == None :
         return False    
     for ind, row in note_ind:
         if ind == index:
             information('Вы действительно хотите удалить заметку' )
             result = input(f' {row[0]} {row[1]} {row[2]} ? (y/n)').lower()
-                        #   ' {row[0]} {row[1]} {row[2]} ? (y/n)').lower()
             break
     if result in 'нНYy':
         return True
@@ -141,24 +102,16 @@ def select_note(message:str):
     return note
 
 
-def change_note():
-    print('Введите новые данные (если изменения не требуютcя, нажмите Enter')
+def change_note(note:list):
+    print('Введите новые данные (если изменения не требуютcя, нажмите Enter)')
     date_note = ""
     y_n = input("Изменить дату заметки ? (y/n): ")
     if (y_n in "yYнН") :
         date_note = def_date()
-    # last_name = input('Введите фамилию :')
-
-    header = input('Введите заголовок заметки: ')
-    note_body = input('Введите текст заметки: ')
-    # phone = input('Введите телефон:')
-    # comment = input('Введите адрес:')
-    return   date_note, header, note_body  #, phone, comment
+    header = input(f'Введите заголовок заметки ({note[0][1][1]}): ')
+    note_body = input(f'Введите текст заметки ({note[0][1][2]}): ')
+    return   date_note, header, note_body 
  
 
 def information(text:str):
     print(text)
-
-def information_data(text:str):
-    print(f'Введены данные : {text}')
-
